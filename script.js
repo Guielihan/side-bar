f6b049aa21fe3dbdcf1d5175dde48d6653809fff
Guielihan <devguielihan@gmail.com>
2026-01-11 00:02:17 -0300

    Update project: finalizing sidebar updates

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ const lightBtn = document.getElementById('lightModeBtn');
 const darkBtn = document.getElementById('darkModeBtn');
 const body = document.body;
 
-// Sidebar Toggle
 toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
 });
 
-// Light Mode Click
 lightBtn.addEventListener('click', () => {
     body.classList.remove('dark-mode');
     lightBtn.classList.add('active-mode');
     darkBtn.classList.remove('active-mode');
 });
 
-// Dark Mode Click
 darkBtn.addEventListener('click', () => {
     body.classList.add('dark-mode');
     darkBtn.classList.add('active-mode');
